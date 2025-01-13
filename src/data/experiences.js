6a48d8e0f4e61355ca631cf9d96008fedd1a1be2
Niteesh S R <104364679+GodOfZap@users.noreply.github.com>
2025-01-13 19:19:19 +0530

    Add files via upload

---
+++ src/data/experiences.js
@@ -0,0 +1,45 @@
+import lenovo from "../assets/images/lenovo.jpeg";
+import randis from "../assets/images/randis.jpg";
+import iil from "../assets/images/iil.jpeg";
+import stealth from "../assets/images/stealth.jpeg";
+
+import { blue, green, pink, yellow } from "../utils";
+
+export const experiences = [
+  {
+    logo: stealth,
+    name: "Current Focus",
+    joined: "Sept'24",
+    end: "Present",
+    title: "Open Source Contributor",
+    bio: "Pursuing certifications in CCNA and Security+, while working on various full stack projects. Responsibilities include researching network security topics developing RESTful APIs and implementing user authentication systems.",
+    color: blue,
+  },
+  {
+    logo: iil,
+    name: "Intrust Innovation Labs",
+    joined: "Jan'24",
+    end: "July'24",
+    title: "Cryptograhic Intern",
+    bio: "Brainstormed various ideas with mentors and teammates for the PoC. Assisted in developing website frontend and Developed libraries and maintained web applications using Spring focusing on both front-end and back-end",
+    color: green,
+  },
+  {
+    logo: randis,
+    name: "RANDIS",
+    joined: "Sept'22",
+    end: "Jan'24",
+    title: "Team Member",
+    bio: "In Pondicherry University, we developed collaborative skills in a team while builting amazing projects and brainstormed on various problem statements",
+    color: pink,
+  },
+  {
+    logo: lenovo,
+    name: "Lenovo",
+    joined: "Aug'21",
+    end: "Sept'21",
+    title: "In-Plant Trainee",
+    bio: "Worked on various roles in hardwares from assembling computer parts to troubleshooting hardware and software issues",
+    color: yellow,
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+import stonks from "../assets/images/portfolio.png";
+import srms from "../assets/images/srms.png";
+import ayush from "../assets/images/ayush.png";
+import cf from "../assets/images/cf.png";
+import a2048 from "../assets/images/a2048.jpg";
+import sec from "../assets/images/sec.png";
+
+export const projects = [
+  {
+    title: "Ayush Portal",
+    image: ayush,
+    github: "https://github.com/GodOfZap/Ayush/",
+    description:
+      "The AYUSH Hub unites startups, investors, and government entities in a centralized platform for AYUSH projects.",
+  },
+  {
+    title: "Smart Result Management System",
+    description:
+      "A web application in which the user can find there marks and the teacher's can give results.",
+    image:srms,
+        github: "https://github.com/GodOfZap/Student-Result-Management-System/",
+  },
+  {
+    title: "Portfolio v1",
+    description: "Built using ReactJS, ThreeJS, CSS3 and lots of ❤️ love.",
+    image:stonks,
+    github: "https://github.com/GodOfZap/Portfolio-v1",
+  },
+  {
+    title: "Secure Cloud File Storage",
+    description: "Built a node CLI interface and an web app which supports most cloud platforms.",
+    image:cf,
+     link: "https://github.com/GodOfZap/Cloud-Fortress",
+  },
+  {
+    title: "Offline mobile games",
+    description: "Built several offline moblile games like 2048, tic tac toe, snake games.",
+    image:a2048,
+  },
+  {
+    title: "Testing of cryptographic algorithms",
+    description: "Formal Testing of Algorithm through which the validity and integrity was accessed.",
+    image:sec,
+     link: "https://docs.google.com/presentation/d/1dBK4kWiODB7epSuRjdVOAIFZfGHnTqhK/edit?usp=sharing&ouid=103701871477822752350&rtpof=true&sd=true",
+  },
+];
